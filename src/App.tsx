17c8a2a78a0e5eca707461a53e8ee951acf12f46
Dominik Windorpski <windorpskidominik2204@gmail.com>
2025-12-04 17:20:43 +0100

    feature: mode switch component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import CalculatorWrapper from "./components/CalculatorWrapper";
+import ModeSwitchBar from "./components/ModeSwitchBar";
+import type { CalculatorMode } from "@/lib/types";
+import { useState } from "react";
 
 export default function App() {
+  const [mode, setMode] = useState<CalculatorMode>("standard");
+
   return (
     <main className="flex items-center justify-center h-screen w-screen">
       <CalculatorWrapper>
-        {/* Calculator Content */}
-        <></>
+        <ModeSwitchBar mode={mode} setMode={setMode} />
       </CalculatorWrapper>
     </main>
   );
---
+++ src/components/ModeSwitchBar.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import type { CalculatorMode } from "@/lib/types";
+
+type ModeSwitchBarProps = {
+  mode: CalculatorMode;
+  setMode: (mode: CalculatorMode) => void;
+};
+
+export default function ModeSwitchBar({ mode, setMode }: ModeSwitchBarProps) {
+  return (
+    <div className="flex items-center gap-2 w-full">
+      <Button size="lg" variant={mode === "standard" ? "default" : "secondary"} onClick={() => setMode("standard")}>
+        Standard
+      </Button>
+      <Button size="lg" variant={mode === "scientific" ? "default" : "secondary"} onClick={() => setMode("scientific")}>
+        Scientific
+      </Button>
+    </div>
+  );
+}
