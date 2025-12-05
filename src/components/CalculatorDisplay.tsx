35775d845fc66f53c9ce6f11378559ab147f8d69
Dominik Windorpski <windorpskidominik2204@gmail.com>
2025-12-05 16:53:11 +0100

    style: changed display styling

--- src/components/CalculatorDisplay.tsx
+++ src/components/CalculatorDisplay.tsx
@@ -1,6 +1,6 @@
 export default function CalculatorDisplay({ value }: { value: string; }) {
   return (
-    <div className="text-right text-3xl font-bold w-full bg-gray-100 rounded-md border border-gray-200 p-3 flex items-center justify-end whitespace-nowrap tracking-wide">
+    <div className="text-right text-4xl w-full bg-gray-100 rounded-md border border-gray-200 p-4 flex items-center justify-end whitespace-nowrap tracking-wide">
       {value}
     </div>
   );
