3af938777a1d9d390844a27559a6d7250fea831a
Dominik Windorpski <windorpskidominik2204@gmail.com>
2025-12-06 12:09:42 +0100

    refactor: keypad buttons variant

--- src/components/CalculatorButton.tsx
+++ src/components/CalculatorButton.tsx
@@ -3,13 +3,12 @@ import { Button } from "@/components/ui/button";
 type CalculatorButtonProps = {
   children: React.ReactNode;
   className?: string;
-  variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link";
   onClick: () => void;
 };
 
-export default function CalculatorButton({ children, className, variant, onClick }: CalculatorButtonProps) {
+export default function CalculatorButton({ children, className, onClick }: CalculatorButtonProps) {
   return (
-    <Button className={"aspect-square text-3xl w-full p-10 " + className} variant={variant} onClick={onClick} >
+    <Button className={"aspect-square text-3xl w-full p-10 " + className} variant="outline" onClick={onClick} >
       {children}
     </Button >
   );
