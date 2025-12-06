409e0b90422e03e0e681cac7de8d16ce2931d45d
Dominik Windorpski <windorpskidominik2204@gmail.com>
2025-12-06 12:18:53 +0100

    fix: wrapper max width

--- src/components/CalculatorWrapper.tsx
+++ src/components/CalculatorWrapper.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent } from "@/components/ui/card";
 
 export default function CalculatorWrapper({ children }: { children: React.ReactNode; }) {
   return (
-    <Card className="w-full max-w-md">
+    <Card className="w-full max-w-lg">
       <CardContent className="flex flex-col gap-4">
         {children}
       </CardContent>
