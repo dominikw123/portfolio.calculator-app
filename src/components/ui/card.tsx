5e0f06ce51e058225f47dc23e90985c9ac270cac
Dominik Windorpski <windorpskidominik2204@gmail.com>
2025-12-09 16:32:08 +0100

    style: removed rounded borders for mobile view

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card"
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl sm:border py-6 shadow-sm dark:border-input",
+        "bg-card text-card-foreground flex flex-col gap-6 sm:rounded-xl sm:border py-6 shadow-sm dark:border-input",
         className
       )}
       {...props}
